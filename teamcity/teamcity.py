90562beb635198503f34e3012d48ea8315b3a74b
Yunlin Tan <yunlin.tan@mihoyo.com>
2022-11-24 20:37:41 +0800

    Update get_all_builds methods

--- teamcity/teamcity.py
+++ teamcity/teamcity.py
@@ -45,19 +45,26 @@ from teamcity.teamcity_const import AUTH_METHOD
 
 class TeamCity:
     def __init__(self, server=None, tokens=None, user=None, password=None, guest=False):
-        self.server = server or os.environ.get('TEAMCITY_SERVER', None)
+        self.server = self.process_server_address(server or os.environ.get('TEAMCITY_SERVER', None))
         self.tokens = tokens or os.environ.get('TEAMCITY_TOKENS', None)
         self.user = user or os.environ.get('TEAMCITY_USER', None)
         self.password = password or os.environ.get('TEAMCITY_PASSWORD', None)
         self.guest = guest or os.environ.get('TEAMCITY_GUEST', False)
-        self.authentication_method, self.base_url, self.header = self.check_auth_method(server)
+        self.authentication_method, self.base_url, self.header = self.check_auth_method()
         self.session = requests.Session()
+
+    @staticmethod
+    def process_server_address(server):
+        """Process server address."""
+        if not server:
+            logging.error('Failed to detect TeamCity server.')
+            raise ValueError('TeamCity server is not set.')
+
+        server = server[:-1] if server.endswith('/') else server
+        return server if server.startswith('http://') or server.startswith('https://') else f'https://{server}'
 
     def check_auth_method(self):
         """Check authentication method and return related info."""
-        if not self.server:
-            logging.error('Failed to detect TeamCity server.')
-            raise ValueError('TeamCity server is not set.')
 
         header = {'Accept': 'application/json'}
         if self.tokens:
@@ -83,6 +90,7 @@ class TeamCity:
     def request_base(self, url, method, extra_headers={}, data=None, timeout=60, retries=3):
         url, headers = f'{self.base_url}/{url}', self.header
         headers.update(extra_headers)
+        logging.info(f'Calling TeamCity API: {url}')
 
         while retries > 0:
             try:
@@ -99,16 +107,31 @@ class TeamCity:
                     raise ValueError('Unknown authentication method.')
 
             except Exception as e:
-                logging.error(f'Failed to post request to TeamCity: {e}, retries left: {retries}')
+                logging.error(f'Failed to {method.lower()} request to TeamCity: {e}, retries left: {retries}')
 
             if response is None or response.status_code != 200:
-                logging.error(f'Failed to post request to TeamCity: {response.status_code}, retries left: {retries}')
+                logging.error(
+                    f'Failed to {method.lower()} request to TeamCity: {response.status_code}, retries left: {retries}')
             else:
                 return response
 
             retries -= 1
+        else:
+            logging.error(f'No retries left, failed to {method.lower()} request to TeamCity: {response.status_code}')
+            raise ValueError(f'No retries left, failed to {method.lower()} request to TeamCity: {response.status_code}')
 
     def post_request(self, url, extra_headers={}, data=None, timeout=60, retries=3):
         extra_headers.update({'Content-Type': 'application/json'})
         return self.request_base(url=url, method='POST', extra_headers=extra_headers, data=data, timeout=timeout,
                                  retries=retries)
+
+    def get_request(self, url, extra_headers={}, data=None, timeout=60, retries=3):
+        extra_headers.update({'Accept': 'application/json'})
+        return self.request_base(url=url, method='GET', extra_headers=extra_headers, data=data, timeout=timeout,
+                                 retries=retries).json()
+
+    def get_all_builds(self, build_type_id, max_results=10000):
+        """Get builds by build type id from TeamCity."""
+        url = f'builds?locator=defaultFilter:false,buildType:(id:{build_type_id}),count:{max_results}'
+        response = self.get_request(url)
+        return response['build']
